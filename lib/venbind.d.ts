2afe14d77b08d54ab97070c2ae5d3000e567cbf7
Tuxinal <24763016+tuxinal@users.noreply.github.com>
2025-04-21 23:46:19 +0330

    h

--- lib/venbind.d.ts
+++ lib/venbind.d.ts
@@ -2,6 +2,7 @@ export class Venbind {
   startKeybinds(callback: (id: string, keyup: boolean) => void, app_id: string | null): void;
   setKeybinds(keybinds: KeybindInfo[]): void;
   defineErrorHandle(callback: (error: string) => void): void;
+  getCurrentShortcut(): string;
 }
 export interface KeybindInfo {
   id: string
